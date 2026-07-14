6f583f45610e0a4a8fa86411085d65927a4fa8fd
manish0345368 <palwal8930@gmail.com>
2026-07-14 12:05:19 +0530

    Fix reload issue

--- app.py
+++ app.py
@@ -1,45 +1,41 @@
-from flask import Flask, render_template, request, redirect, url_for
-# from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
 
-
 app = Flask(__name__)
+app.secret_key = "manish123"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-
     if request.method == "POST":
 
         name = request.form["name"]
         dob = datetime.strptime(request.form["dob"], "%Y-%m-%d").date()
 
         today = date.today()
-
         age = relativedelta(today, dob)
 
-        years = age.years
-        months = age.months
-        days = age.days
-
         total_days = (today - dob).days
-        hours = total_days * 24
-        minutes = hours * 60
-        seconds = minutes * 60
 
-        return render_template(
-            "index.html",
-            name=name,
-            dob=dob,
-            today=today,
-            years=years,
-            months=months,
-            days=days,
-            total_days=total_days,
-            hours=hours,
-            minutes=minutes,
-            seconds=seconds
-        )
+        session["result"] = {
+            "name": name,
+            "dob": dob.strftime("%d/%m/%Y"),
+            "today": today.strftime("%d/%m/%Y"),
+            "years": age.years,
+            "months": age.months,
+            "days": age.days,
+            "total_days": total_days,
+            "hours": total_days * 24,
+            "minutes": total_days * 24 * 60,
+            "seconds": total_days * 24 * 60 * 60
+        }
+
+        return redirect(url_for("home"))
+
+    result = session.pop("result", None)
+
+    if result:
+        return render_template("index.html", **result)
 
     return render_template("index.html")
 
